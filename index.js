fdec89d7a813e9496cee4962e92a1d38733f420b
ashish159565 <68137314+ashish159565@users.noreply.github.com>
2024-03-02 01:53:39 +0530

    Making changes to index file

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-
-
-
-
 ScrollReveal({
-    reset:true,
-    distance:'70px',
-    duration:2500,
-    delay:200
+    reset: true,
+    distance: '70px',
+    duration: 2500,
+    delay: 200
 });
 
-ScrollReveal().reveal('.box-1111, .sponsers-section ',{delay:200, origin:'left', mobile: false});
-ScrollReveal().reveal('.Hero-page-img, .speakers-heading, .about-heading',{delay:200, origin:'right', mobile: false});
-ScrollReveal().reveal('.slider-container, .text-box, .map-registration',{delay:200, mobile: false});
+ScrollReveal().reveal('.box-1111, .sponsers-section ', { delay: 200, origin: 'left', mobile: false });
+ScrollReveal().reveal('.Hero-page-img, .speakers-heading, .about-heading', { delay: 200, origin: 'right', mobile: false });
+ScrollReveal().reveal('.slider-container, .text-box, .map-registration', { delay: 200, mobile: false });
 
 
 
@@ -22,7 +18,7 @@ ScrollReveal().reveal('.slider-container, .text-box, .map-registration',{delay:2
 
 // Map
 
-map.on('load', function () {
+map.on('load', function() {
     map.resize();
 });
 
@@ -33,18 +29,17 @@ map.on('load', function () {
 
 show = () => {
 
-	if (document.getElementById("brdrgrp").style.display === "none") {
-		brdrgrp = document.getElementById("brdrgrp");
-		brdrgrp.style.display = "block";
-		brdrgrp.style.marginTop = "5px";
-		document.getElementById("participateNow").classList.remove('bi-arrow-down');
-		document.getElementById("participateNow").classList.add('bi-x-lg');
-	} else {
-		document.getElementById("brdrgrp").style.display = "none";
-		document.getElementById("participateNow").classList.remove('bi-x-lg');
-		document.getElementById("participateNow").classList.add('bi-arrow-down');
-	}
+    if (document.getElementById("brdrgrp").style.display === "none") {
+        brdrgrp = document.getElementById("brdrgrp");
+        brdrgrp.style.display = "block";
+        brdrgrp.style.marginTop = "5px";
+        document.getElementById("participateNow").classList.remove('bi-arrow-down');
+        document.getElementById("participateNow").classList.add('bi-x-lg');
+    } else {
+        document.getElementById("brdrgrp").style.display = "none";
+        document.getElementById("participateNow").classList.remove('bi-x-lg');
+        document.getElementById("participateNow").classList.add('bi-arrow-down');
+    }
 
 
 }
-
